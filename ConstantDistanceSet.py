ad3ec1f09ef3f82530ce55c37a92524a13b65460
Quentin Bechet <quentin.bechet@gmail.com>
2024-02-22 15:36:55 +1100

    Adding thr distance split method to ConstantDistanceSet

--- ConstantDistanceSet.py
+++ ConstantDistanceSet.py
@@ -10,7 +10,24 @@ import utils
 
 class ConstantDistanceSet(Set):
 
+    # Object initialisation
     def __init__(self, distance):
         super().__init__(distance)
+
+    # Method to split the set into a given distance
+    def setBlockDistances(self): 
+
+        optionDistanceBlock = []
+
+        for distance in np.arange(globals.minBlockDistance, self.distance + globals.stepBlockDistance, globals.stepBlockDistance):
+            if self.distance / distance == np.floor(self.distance / distance):
+                optionDistanceBlock.append(distance)
+
+        # Setting then randomly the distance
+        blockDistance = optionDistanceBlock[np.random.randint(low=0, high=len(optionDistanceBlock))]
+
+        # Finally setting the list of distance blocks
+        for i in np.arange(int(self.distance / blockDistance)):
+            self.listBlockDistance.append(blockDistance)
 
     
