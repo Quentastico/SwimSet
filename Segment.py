edb944730c0ee046787caf2d5b7b0179a334a283
Quentin Bechet <quentin.bechet@gmail.com>
2024-02-29 14:32:47 +1100

    adding a functionality to replace None" by None in Segment

--- Segment.py
+++ Segment.py
@@ -122,6 +122,16 @@ class Segment:
       else:
         parameterValues[parameter] = None
 
+    # 3. Finally, just for consistency with other parts of the code, we replace the "None" into actual None
+    for parameter in parameterValues.keys():
+      if parameterValues[parameter] is not None:
+        # Case where there is only one value that is equal to "None"
+        if (len(parameterValues[parameter]) == 1) & (parameterValues[parameter][0] == "None"):
+          parameterValues[parameter] = None
+        # Case where there is multiple values
+        if len(parameterValues[parameter]) > 1:
+          parameterValues[parameter] = parameterValues[parameter][parameterValues[parameter] == "None"] = None
+
     return parameterValues
 
   # Copy method
