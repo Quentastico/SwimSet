36c05564e90fd352be6ea5b95cdb9cd492e5a156
Quentin Bechet <quentin.bechet@gmail.com>
2024-03-18 17:34:44 +1100

    typo

--- IncreasingDecreasingDistanceSet.py
+++ IncreasingDecreasingDistanceSet.py
@@ -151,7 +151,7 @@ class IncreasingDecreasingDistanceSet(Set):
                                                             parameterValue=variationSegment.selParameterVariation[indexBlock])
                     
                     # Getting the constraits created by the changing segment on the non-changing segment
-                    constraintBaseSegment = block.listSegment[changingSegmentIndex].getBaseSegmentParameters(selParamater=variationSegment.selParameter)
+                    constraintBaseSegment = block.listSegment[changingSegmentIndex].getBaseSegmentParameters(selParameter=variationSegment.selParameter)
 
                     # Changing all the parameters values in the non-changing segment
                     for parameter in globals.listAllParameters:
