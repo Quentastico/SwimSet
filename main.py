1b79d0e6b30831e884729e844252523c2c4e089d
Quentin Bechet <quentin.bechet@gmail.com>
2024-05-27 10:33:30 +1000

    creating the globals and making is a global variable

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ import functions_framework
 import json
 import urllib
 from Training import Training
+from utils import extractFromJSON
+from Globals import Globals
 
 @functions_framework.http
 def generateTraining(req):
 
     # Extracting the data from the json
-    request_json = req.get_json(silent=True)
-    request_args = req.args
+    requestJSON = req.get_json(silent=True)
+    requestArgs = req.args
 
     # Extraction of the distance
-    if request_json and 'distance' in request_json:
-        distance = float(request_json['distance'])
-    elif request_args and 'distance' in request_args:
-        distance = float(request_args['distance'])
+    distance = extractFromJSON(parameterName="distance",
+                               parameterDefault=None,
+                               requestJSON=requestJSON,
+                               requestArgs=requestArgs)
+    
+    # Creation of the parameter object "Globals"
+    global globals
+    globals = Globals()
+    globals.buildFromReq(requestJSON=requestJSON,
+                         requestArgs=requestArgs)
 
     # Creation of a training function
-    training = Training(distance=distance, standardInit=True, verbose=0)
+    training = Training(distance=distance,
+                        standardInit=True,
+                        verbose=0)
 
     # Generation of the training output
     output = training.dictionary()
